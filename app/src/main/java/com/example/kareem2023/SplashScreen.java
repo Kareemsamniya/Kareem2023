e11898e25e202fb98e0aed1a4a8ee06cad01960c
MahmodAli25 <mahmod.alia620@gmail.com>
2023-11-01 14:12:22 +0200

    to do

---
+++ app/src/main/java/com/example/kareem2023/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.example.kareem2023;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private TextView etSplashWelcome;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        etSplashWelcome= findViewById(R.id.etSplashWelcome);
+        //start next activity (screen) automatically  after period of time
+        Handler h = new Handler();
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                //to open new activity from current to next
+                Intent i = new Intent(SplashScreen.this, SignInActivity.class);
+                startActivity(i);
+                //to close current activity
+                finish();
+            }
+        };
+        h.postDelayed(r, 3000);
+    }
+}
